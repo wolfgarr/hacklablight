4556380ff24ae16063b341f93caccbe3a40f4986
Mikael Kunnari <mikaelkunnari@gmail.com>
2019-10-18 16:35:08 +0300

    Add mock configuration file

---
+++ conf.h
@@ -0,0 +1,11 @@
+#ifndef LIGHTCONF_H
+#define LIGHTCONF_H
+
+const char* ssid = "MY_WIFI_SSID";
+const char* pass = "MY_WIFI_PASSWORD";
+
+const char* url = "https://example.com:1234";
+
+const char* httpsFingerprint = "read README.md for instructions";
+
+#endif
